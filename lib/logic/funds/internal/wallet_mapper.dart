e6694496eb7eee21e84d47128e8b20a6b754b54c
Sam Komarov <sam@skomarov.com>
2022-07-23 08:19:22 +0300

    implement WalletMapper

---
+++ lib/logic/funds/internal/wallet_mapper.dart
@@ -0,0 +1,10 @@
+import 'package:avencia/logic/funds/internal/entities.dart';
+
+class WalletMapper {
+  Wallet fromJson(Map<String, dynamic> json) {
+    final jsonWallet = json["wallet"] as Map<String, dynamic>;
+    return jsonWallet.map(
+      (curr, bal) => MapEntry(curr, bal.toDouble()),
+    );
+  }
+}
---
+++ test/logic/funds/internal/wallet_mapper_test.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:avencia/logic/funds/internal/entities.dart';
+import 'package:avencia/logic/funds/internal/wallet_mapper.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../fixtures/fixture_reader.dart';
+
+void main() {
+  test("should convert a valid fixture to a wallet entity", () async {
+    // arrange
+    final tJson = json.decode(fixture("wallet.json"));
+    const Wallet wantWallet = {
+      "USD": 1200.5,
+      "RUB": 30000,
+      "BTC": 0.001,
+    };
+    // act
+    final result = WalletMapper().fromJson(tJson);
+    // assert
+    expect(result, wantWallet);
+  });
+}
