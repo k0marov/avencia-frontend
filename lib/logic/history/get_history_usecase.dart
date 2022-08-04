9c6c9e40a02ab1eefef937cd7621fbe203d51b87
Sam Komarov <sam@skomarov.com>
2022-08-04 07:58:44 +0300

    implement the get history use case

--- lib/logic/history/get_history_usecase.dart
+++ lib/logic/history/get_history_usecase.dart
@@ -1,1 +1,17 @@
+import 'package:avencia/logic/auth/auth_http_client.dart';
+import 'package:avencia/logic/core/network_usecase.dart';
+import 'package:avencia/logic/history/internal/entities.dart';
+import 'package:avencia/logic/history/internal/history_mapper.dart';
+import 'package:dartz/dartz.dart';
 
+import '../../config/const.dart';
+
+typedef GetHistoryUseCase = Future<Either<Exception, History>> Function(void);
+
+GetHistoryUseCase newGetHistoryUseCase(AuthHTTPClient client, HistoryMapper mapper,) => newBaseNetworkUseCase(
+      inpMapper: NoInpMapper(),
+      getUri: (_) => Uri.https(apiHost, "", {}),
+      method: "GET",
+      client: client,
+      outMapper: mapper,
+    );
