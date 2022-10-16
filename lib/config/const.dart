7e2d02d8275faff3f010f14c461f27b4b6ac00f0
Sam Komarov <sam@skomarov.com>
2022-10-16 18:10:55 +0300

    temporarily add helpers as a local package

--- lib/config/const.dart
+++ lib/config/const.dart
@@ -1,11 +1,11 @@
 const apiHost = "avencia.skomarov.com";
 
-const transferEndpoint = "/v1/app/transfer/";
-const getUserInfoEndpoint = "/v1/app/user/";
-const historyEndpoint = "/v1/app/history/";
-const userDetailsEndpoint = "/v1/app/user/details/";
-const passportEndpoint = "/v1/app/user/kyc/passport/";
-const passportBackEndpoint = "${passportEndpoint}back/";
-const passportFrontEndpoint = "${passportEndpoint}front/";
+const transferEndpoint = "/v1/app/transfer";
+const getUserInfoEndpoint = "/v1/app/user";
+const historyEndpoint = "/v1/app/history";
+const userDetailsEndpoint = "/v1/app/user/details";
+const passportEndpoint = "/v1/app/user/kyc/passport";
+const passportBackEndpoint = "$passportEndpoint/back";
+const passportFrontEndpoint = "$passportEndpoint/front";
 
-const genTransactionCodeEndpoint = "/v1/app/gen-transaction-code/";
+const genTransactionCodeEndpoint = "/v1/app/gen-transaction-code";
