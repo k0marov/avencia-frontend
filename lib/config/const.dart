d749f5bec54122cc1b22cbe8093be86254d6b8a5
Sam Komarov <sam@skomarov.com>
2022-10-16 16:30:38 +0300

    implement the first iteration on the PassportCubit

--- lib/config/const.dart
+++ lib/config/const.dart
@@ -4,5 +4,8 @@ const transferEndpoint = "/v1/app/transfer/";
 const getUserInfoEndpoint = "/v1/app/user/";
 const historyEndpoint = "/v1/app/history/";
 const userDetailsEndpoint = "/v1/app/user/details/";
+const passportEndpoint = "/v1/app/user/kyc/passport/";
+const passportBackEndpoint = "${passportEndpoint}back/";
+const passportFrontEndpoint = "${passportEndpoint}front/";
 
 const genTransactionCodeEndpoint = "/v1/app/gen-transaction-code/";
