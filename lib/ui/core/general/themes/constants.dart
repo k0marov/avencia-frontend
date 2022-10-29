d4aba558bec17b9887d6e2e784b1d7b13425e488
Sam Komarov <sam@skomarov.com>
2022-10-29 15:55:38 +0300

    fill in the light theme

--- lib/ui/core/general/themes/constants.dart
+++ lib/ui/core/general/themes/constants.dart
@@ -4,10 +4,11 @@ abstract class AppColors {
   static const whiteBG = Color(0xff9C42F5);
   static const whiteCard = Color(0xffEFEFF1);
   static const whiteIcon = Color(0xffEEEFF0);
-  static const whiteText = Color(0xFF515151);
-  static const whiteLabelText = Color(0xFFAAAAB2);
-  static const whiteTitleText = Color(0xFF403D51);
-  static const whiteEmphasisText = Color(0xFF0C0C0C);
+  static const darkText = Color(0xFF515151);
+  static const darkLabelText = Color(0xFFAAAAB2);
+  static const darkTitleText = Color(0xFF403D51);
+  static const darkEmphasisText = Color(0xFF0C0C0C);
+  static const whiteText = Color(0xFFF5EBFE);
   static const green = Color(0xFF51E289);
   static const red = Color(0xFF51E289);
   static const purple = Color(0xFF9C42F5);
--- lib/ui/core/general/themes/light_theme.dart
+++ lib/ui/core/general/themes/light_theme.dart
@@ -1,3 +1,17 @@
 part of 'theme.dart';
 
-ThemeData createLightTheme() => ThemeData();
+ThemeData createLightTheme() => ThemeData(
+      colorScheme: const ColorScheme(
+        brightness: Brightness.light,
+        primary: AppColors.purple,
+        onPrimary: AppColors.whiteText,
+        secondary: AppColors.gradientPink,
+        onSecondary: AppColors.whiteText,
+        error: AppColors.red,
+        onError: AppColors.whiteText,
+        background: AppColors.whiteBG,
+        onBackground: AppColors.darkText,
+        surface: AppColors.whiteCard,
+        onSurface: AppColors.darkText,
+      ),
+    );
