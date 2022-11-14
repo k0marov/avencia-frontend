1196067e7053915fb9871fce2f91652fed6fe808
Sam Komarov <sam@skomarov.com>
2022-11-14 20:36:47 +0300

    implement opening the mail app when pressing support

--- lib/ui/features/navigation_menu/bottom_buttons.dart
+++ lib/ui/features/navigation_menu/bottom_buttons.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../core/widgets/icon_with_text.dart';
 import '../../core/widgets/simple_button.dart';
+
+const supportEmailURL = "mailto:support@avencia.com";
 
 class BottomButtons extends StatelessWidget {
   const BottomButtons({Key? key}) : super(key: key);
@@ -12,7 +15,7 @@ class BottomButtons extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: SimpleButton(
-        onPressed: () {},
+        onPressed: () => launchUrl(Uri.parse(supportEmailURL)),
         contents: IconWithText(
           icon: Icons.question_mark_outlined,
           text: Text(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import firebase_database
 import path_provider_macos
 import shared_preferences_macos
 import sign_in_with_apple
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -23,4 +24,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
